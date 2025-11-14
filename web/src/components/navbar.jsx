7bbc13be1e0224ae48d6e4004a1ab16da59c30fc
Aintharasopa660710744 <aintharasopa_s@su.ac.th>
2025-11-14 10:17:44 +0000

    ConnectDB+Home+NewGame+AddUpdateDeleteGame

--- web/src/components/navbar.jsx
+++ web/src/components/navbar.jsx
@@ -41,15 +41,6 @@ function Navbar() {
         <li className={location.pathname === '/newgame' ? 'active' : ''}>
           <Link to="/newgame">เกมใหม่</Link>
         </li>
-        <li className={location.pathname === '/popular' ? 'active' : ''}>
-          <Link to="/popular">เกมยอดนิยม</Link>
-        </li>
-        <li className={location.pathname === '/promotion' ? 'active' : ''}>
-          <Link to="/promotion">โปรโมชั่น</Link>
-        </li>
-        <li className={location.pathname === '/free' ? 'active' : ''}>
-          <Link to="/free">เกมฟรี</Link>
-        </li>
         <li className={location.pathname === '/contact' ? 'active' : ''}>
           <Link to="/contact">ติดต่อเรา</Link>
         </li>
